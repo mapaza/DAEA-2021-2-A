f3fe69371974f7ffb0448da16ec94944e8c587af
Mariam Apaza Santillana <mariam.apaza@tecsup.edu.pe>
2021-08-20 13:21:42 -0500

    lab01 - parte 1

---
+++ lab01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola! ¿Cómo te llamas? :)");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Mucho gusto "+nombre);
+        }
+    }
+}
