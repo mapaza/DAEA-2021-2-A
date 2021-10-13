3d2d2e9e24105172188c5bcdc2ee53a110f33468
mapaza <mariam.apaza@tecsup.edu.pe>
2021-10-13 13:41:09 -0500

    Lab07- Terminado, con manejo de errores Try-Catch

---
+++ Lab07/Negocio/clsNegPerson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Datos;
+
+namespace Negocio
+{
+    public class clsNegPerson
+    {
+        clsDAOPerson daoPerson = new clsDAOPerson();
+
+        public DataTable GetAll()
+        {
+            return daoPerson.GetAll();
+        }
+
+        public DataTable Busqueda(String Busqueda)
+        {
+            return daoPerson.Busqueda(Busqueda);
+        }
+    }
+}
