b1825e813dff1b97abf58a409c8fd131b2a67ff6
mapaza <mariam.apaza@tecsup.edu.pe>
2021-11-17 00:04:08 -0500

    Lab 13 Tarea Terminada

--- Lab13/Controllers/PersonaController.cs
+++ Lab13/Controllers/PersonaController.cs
@@ -89,8 +89,92 @@ namespace Lab13.Controllers
                                select p).FirstOrDefault();
 
             return View(persona);
-        }
+        }    
 
-       
+
+        [HttpGet]
+        public ActionResult Submit(string tipo, string dato)
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "Av. Evergen 123",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "María",
+                Apellido = "Salas",
+                Direccion = "Av. Progreso 325",
+                FechaNac = Convert.ToDateTime("1995-10-28"),
+                Email = "maria@mail.com"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "Av. Los manzanos 3101",
+                FechaNac = Convert.ToDateTime("1982-02-14"),
+                Email = "carlos@mail.com"
+            });
+
+            Persona persona = new Persona {};
+            string estado;
+           
+            if (tipo.Equals("nombre")) {
+                 persona = (from p in personas
+                                   where p.Nombre == dato
+                                   select p).FirstOrDefault();
+
+                if(persona is null){
+                    
+                    estado = "no_encontrado";
+                }
+                else
+                {
+                    estado = "encontrado";
+                    ViewBag.estado = estado;
+                    ViewBag.id = persona.PersonaID;
+                    ViewBag.nombre = persona.Nombre;
+                    ViewBag.apellido = persona.Apellido;
+                    ViewBag.fecha_nacimiento = persona.FechaNac;
+                    ViewBag.direccion = persona.Direccion;
+                    ViewBag.email = persona.Email;
+                }
+                
+            }
+            else if (tipo.Equals("apellido"))
+            {
+                persona = (from p in personas
+                                   where p.Apellido == dato
+                                   select p).FirstOrDefault();
+                if (persona is null)
+                {
+                    estado = "no_encontrado";
+                }
+                else
+                {
+                    estado = "encontrado";
+                    ViewBag.estado = estado;
+                    ViewBag.id = persona.PersonaID;
+                    ViewBag.nombre = persona.Nombre;
+                    ViewBag.apellido = persona.Apellido;
+                    ViewBag.fecha_nacimiento = persona.FechaNac;
+                    ViewBag.direccion = persona.Direccion;
+                    ViewBag.email = persona.Email;
+                }
+            }
+
+            
+            return View("Busqueda");
+        }
     }
 }
